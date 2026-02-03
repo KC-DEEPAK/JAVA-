1acddd6e10bd37b295dc75772a24d93013d0cbe8
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-02-03 23:33:44 +0530

    Primitive DataTypes

---
+++ Hello.java
@@ -0,0 +1,16 @@
+class Hello
+{
+  public static void main(String a[])
+  {
+    int num1=5;
+    int num2=7;
+    float marks=85.6f;
+    char student='A';
+    long l=5674456l;
+    double num4=12.333333333344;
+    int result=num1+num2;
+    String name="deepak";
+    System.out.println(result+marks+num4);
+    System.out.println("Hello World"+student+name);
+  }
+    }// primitive Data types

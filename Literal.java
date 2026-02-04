f28f144c1fc0f91be3b5cc99ad68780bb2ac0544
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-02-04 19:30:02 +0530

    Literals in Java

---
+++ Literal.java
@@ -0,0 +1,20 @@
+class Literal
+{
+  public static void main(String a[])
+  {
+    int num1=0b1010;//WE can also write in the Binary form ..
+    int num2=012;// we can also print in octal formate 
+    float x=45.7f;// 
+    double y=20.5555;
+    char c='K';
+    System.out.println(c);
+    c++;
+    int num4=10_00_00_00_00;//we can do this in java and we can count  the zeros easy...
+    String name1="DEEPAK";
+    System.out.println(c);
+    System.out.println(num1+y);
+    System.out.println(num2);
+   // System.out.println(name+name1);
+    System.out.println(num4);
+  }
+}

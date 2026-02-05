49026a5178d1ad8997776d05108474c3c2d07c3e
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-02-06 00:22:34 +0530

    Relational Opeartion

---
+++ Relational.java
@@ -0,0 +1,19 @@
+//we come across diffrent classes in Relational operation are..
+//1]Grater,smaller,assignment operator,not equal,equal to
+//symbol >.<,=,!=,==,
+class Relational
+{
+  public static void main(String args[])
+  {
+    int num1=6;
+    int num2=6;
+   // boolean result=num1>num2;
+   //boolean result=num1<num2;
+  // boolean result=num1>=num2;
+  //boolean result=num1<=num2;
+   boolean result=num1==num2;
+   //boolean result=num1!=num2;
+   System.out.println(result);
+
+  }
+} 

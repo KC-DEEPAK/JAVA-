e01433fcc35fc7391f4aa5aa3e5936392e51a4b0
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-02-07 00:45:48 +0530

    CONDITIONAL_STATEMENT(IF-ELSE)

---
+++ Conditional_Statements.java
@@ -0,0 +1,30 @@
+// IN java we have a conditional statements IF & Else stamements 
+class Conditional_Statements
+{
+  public static void main(String args[])
+  {
+    int n=15;
+
+    if(n>10 && n<20)// in between 11 to 
+    {
+      System.out.println("Hai CKM");
+    }
+    else{
+      System.out.println("Bay CKM");
+    }
+    //Another example ..
+    //Compare two number and print which one is grater ..
+    int x=55;
+    int y=10;
+
+    if(x>y)
+    {
+      System.out.println("X is grater");
+      
+    }
+    else
+    {
+      System.out.println("Y is grater");
+    }
+  }
+}
